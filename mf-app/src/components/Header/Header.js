28363b4c26f6f220f496fc3b640c03609b4013b1
Facundo Núñez <facundo.mnunez97@gmail.com>
2022-10-04 12:13:40 -0300

    Creo la app y agrego los componentes Header y Home

---
+++ mf-app/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+        <h1>Mary Flowers</h1>
+        <nav>
+            <ul>
+                <li>Decoraciones</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
