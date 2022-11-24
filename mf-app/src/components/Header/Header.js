2f609e9b6c04d36d9d0ff1f44be7874f0dfde668
Facundo Núñez <facundo.mnunez97@gmail.com>
2022-11-24 18:09:42 -0300

    Cambios6

--- mf-app/src/components/Header/Header.js
+++ mf-app/src/components/Header/Header.js
@@ -1,18 +1,30 @@
 import './Header.css';
 import { Link, NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <header>
-        <h1 className='logo'><Link to='/'>Mary Flowers</Link></h1>
-        <nav>
-            <ul>
-                <li><NavLink to='/decoraciones'>Decoraciones</NavLink></li>
-                <li><NavLink to='/nosotros'>Nosotros</NavLink></li>
-                <li><a href='#footer'>Contacto</a></li>
-            </ul>
-        </nav>
-    </header>
+    <Navbar expand="md" sticky="top" id="header">
+      <Container>
+        <Navbar.Brand id='logo'><Link to='/'>Mary Flowers</Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      </Container>
+      <Container></Container>
+      <Container></Container>
+      <Container></Container>
+      <Container></Container>
+      <Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link><NavLink to='/decoraciones'>Decoraciones</NavLink></Nav.Link>
+            <Nav.Link><NavLink to='/nosotros'>Nosotros</NavLink></Nav.Link>
+            <Nav.Link href="#footer">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>  
   );
 }
 
